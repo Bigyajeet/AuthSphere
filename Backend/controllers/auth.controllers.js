777ccedb2c4bf9bcd2cbb4e18de03e7ae7900bd8
Bigyajeet <bigyajeetkumarpatra@gmail.com>
2026-08-16 17:02:29 +0530

    GitHub OAuth Integration

--- Backend/controllers/auth.controllers.js
+++ Backend/controllers/auth.controllers.js
@@ -44,3 +44,81 @@ export const getProfile=async(req,res)=>{
     res.status(200).json(req.user);
 
 };
+
+export const githubLogin=async(req,res)=>{
+    const githubURL=
+    `https://github.com/login/oauth/authorize`+
+    `?client_id=${process.env.GITHUB_CLIENT_ID}`+
+    `&scope=user:email`;
+    res.redirect(githubURL);
+};
+
+// fallback route for github login
+
+export const githubCallback=async(req,res)=>{
+    try{
+        const {code}=req.query;
+        if(!code){
+            res.status(400).json({
+                message:"No code received from github",
+            });
+        }
+        const tokenResponse=await axios.post(
+            "https://github.com/login/oauth/access_token",
+            {
+                client_id:process.env.GITHUB_CLIENT_ID,
+                client_secret:process.env.GITHUB_CLIENT_SECRET,
+                code,
+
+            },{
+                headers:{
+                    Accept:"application/json",
+                },
+            },
+        );
+      const accessToken=tokenResponse.data.access_token;  
+      //user info fetch from github
+      const userResponse=await axios.get("https://api.github.com/user",{
+        headers:{
+            Authorization:`Bearer ${accessToken}`,
+        },
+      });
+       const githubUser=userResponse.data;
+      //store 
+       const emailResponse=await axios.get("https://api.github.com/user/emails",{
+        headers:{
+            Authorization:`Bearer ${accessToken}`,
+        },
+      });
+
+      const primaryEmail=emailResponse.data.find((email)=>email.primary===true);
+      const email=primaryEmail?.email;
+      if(!email){
+        res.status(400).json({
+            message:"No email found for this github user",
+        })
+      }
+     const user=await User.findOne({email});
+     if(!user){
+        user=await User.create({
+            name:githubUser.name || githubUser.login,
+            email,
+            provider:"github",
+            githubId:githubUser.id.toString(),
+        })
+     }
+     const token=jwt.sign({
+        user:user._id,
+     },
+    process.env.JWT_SECRET,{
+        expiresIn:'7d',
+    });
+    res.redirect(`http://localhost:5173/github-success?token=${token}`);
+         
+    }catch(error){
+        res.status(500).json({
+            message:"Github login Failed",
+            
+        });
+    }
+}
--- Backend/routes/auth.routes.js
+++ Backend/routes/auth.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { googleLogin } from '../controllers/auth.controllers.js';
-import { getProfile } from '../controllers/auth.controllers.js';
+import { getProfile,githubLogin,githubCallback } from '../controllers/auth.controllers.js';
 import { protect } from '../middleware/authMiddleware.js';
 const router=express.Router();
 
 router.post('/google-login',googleLogin);
 router.get('/profile',protect,getProfile);
-
+router.get('/github',githubLogin);
+router.get('/github/callback',githubCallback);
 export default router;
